a56d1e6476542ad5bc383721ab73a658624cc639
AppArcade <144055367+AppArcade@users.noreply.github.com>
2023-09-04 23:25:51 +0530

    Update CVscript.js

--- CVscript.js
+++ CVscript.js
@@ -1,16 +1,1 @@
-let connect = document.querySelector('#connect')
-let about = document.querySelector('#about')
-let projects = document.querySelector('#projects')
 
-ptitle = document.querySelector('#projectstitle')
-connevtview = document.querySelector('#connect_t')
-
-projects.addEventListener('click', function(){
-    ptitle.scrollIntoView({behavior: "smooth", block: "start"});
-})
-
-connect.addEventListener('click', function(){
-    connevtview.scrollIntoView({behavior: "smooth", block: "start"});
-})
-
-console.log("called")
